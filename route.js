3d766d112c8e3d5092a8de56fe2370eb90676e55
jinyoung1129 <jy11290@naver.com>
2020-12-02 15:21:20 +0900

    Test Board Insert, Select

--- route.js
+++ route.js
@@ -3,37 +3,42 @@ const router = express.Router();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
+const sequelize = require('sequelize');
 
-router.use(bodyParser.urlencoded({ extended: false }));
+// DB 연결
+const mysqlConObj = require('./config/mysql');
+const db = mysqlConObj.init();
+mysqlConObj.open(db);
 
-// INSERT
-router.get("/insert", (req, res) => {
-    const test_code = "GA001"
-    const test_name = "쿼리테스트다"
+// BOARD INSERT
+router.post("/insert", (req, res) => {
+    const dataObj = {
+        post_title: "글 작성 테스트",
+        post_contents: "글 작성 테스트 글 내용",
+        wrt_date: new Date(),
+        view_cnt: 1,
+        reply_yn: "Y",
+        major_name: "컴퓨터공학과",
+        subject_name: "피프로젝트",
+        professor_name: "황희정",
+        user_id: "jy11290"
+    };
 
-    con.query('insert into TEST values (?,?)', [test_code, test_name], () => {
-        console.log("insert 완료");
+    const sql = "INSERT INTO BOARD SET ? "
+
+    db.query(sql, dataObj, (err, result) => {
+        if(err) console.log("insert err : ", err);
+        else console.log("insert result : ", result);
     })
-})
-
-// SELECT
-router.get("/select", (req, res) => {
-    con.query('select * from TEST', (err, result) => {
-        res.send(result);
-    })
-})
-
-const con = mysql.createConnection({
-    host: process.env.host,
-    port: process.env.port,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database 
 });
 
-con.connect(err => {
-    if(err) console.log(err);
-    else console.log("MySQL Connected");
+// BOARD SELECT
+router.get("/select", (req, res) => {
+    const sql = "SELECT * FROM BOARD;"
+    db.query(sql, (err, results) => {
+        console.log("results: ", results);
+    })
 })
+
 
 module.exports = router;
