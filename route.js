69710e52d990e9144e2acb77dd2f25b1b03bce46
jinyoung1129 <jy11290@naver.com>
2020-12-03 14:06:31 +0900

    Add Function
    BOARD 테이블 글 수정 기능 추가

--- route.js
+++ route.js
@@ -7,7 +7,7 @@ const mysqlConObj = require('./config/mysql');
 const db = mysqlConObj.init();
 mysqlConObj.open(db);
 
-// BOARD INSERT - 과목게시판 새 글 작성
+// BOARD CREATE - 과목게시판 새 글 작성
 // 클라이언트는 body에 post_title, post_contents, reply_yn, major_name, subject_name, professor_name, user_id를 전달
 router.post("/insert", (req, res) => {
     const dataObj = {
@@ -36,7 +36,7 @@ router.post("/insert", (req, res) => {
     })
 });
 
-// BOARD SELECT - 과목게시판 전체 글 조회
+// BOARD READ - 과목게시판 전체 글 조회
 // 클라이언트에서 과목명/교수명을 파라미터로 전달하면 해당하는 튜플을 전송
 router.get("/select/:subject/:professor", (req, res) => {
     console.log("select");
@@ -55,6 +55,22 @@ router.get("/select/:subject/:professor", (req, res) => {
     })
 })
 
+// BOARD UPDATE - 과목게시판 내 석택한 글 수정
+// 클라이언트에서 post_no을 파라미터로 전달하면 해당 튜블의 post_title, post_contents를 수정한다.
+// 수정할 글 제목과 글 내용은 body를 통해 전달된다.
+router.post("/edit/:id", (req, res) => {
+    const sql = "UPDATE BOARD SET post_title = ?, post_contents = ? where post_no = ?";
+    db.query(sql, [req.body.post_title, req.body.post_contents, req.params.id], (err) => {
+        if (err) {
+            console.log("update err : ", err);
+        }
+        else {
+            console.log("글 수정 성공");
+            res.send("글 수정 성공")
+        }
+    })
+})
+
 // BOARD DELETE - 과목게시판 내 선택한 글 삭제
 // 클라이언트에서 post_no을 전달하면 해당 튜플을 삭제한다.
 router.get("/delete/:id", (req, res) => {
@@ -69,6 +85,8 @@ router.get("/delete/:id", (req, res) => {
         }
     })
 })
+
+
 
 // BOARD SELECT - BOARD 테이블 전체 조회(테스트용)
 router.get("/select", (req, res) => {
