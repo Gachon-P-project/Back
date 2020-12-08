d450cc583f1598a81ba5427fc1ab23405bdc29dd
Seonghyunsun <leaf1993@naver.com>
2020-12-08 23:29:06 +0900

    fix: Edit notice.js to include import notice

--- notice.js
+++ notice.js
@@ -9,29 +9,76 @@ const crawler = async() => {
     
     let $ = cheerio.load(html);
 
+    // 페이지 데이터 저장 객체 생성
+    let num = '';
+    let board_no = '';
+    let title = '';
+    let file = '';
+    let date = '';
+    let view = '';
+    const page = new Array();
+
     try {
         $('.boardlist table tbody tr').each((index, data) => {
-            if (index >= 3){
-                const td = $(data).find('td')
-                td.each((index, data)=>{
-                    /*
-                        0 : 번호
-                        1 : 제목
-                        2 : 작성자(학과)
-                        3 : 첨부파일 이미지
-                        4 : 작성일
-                        5 : 조회수
-                    */
-                    if(index == 1){
-                        console.log($(data).text().trim());
+            const td = $(data).find('td');
+            td.each((i, element)=>{
+                /*
+                    0 : 번호 => [공지]면 length 1, 아니면 0
+                    1 : 제목
+                    2 : 작성자(학과)
+                    3 : 첨부파일 이미지 => 없으면 length 0, 있으면 1
+                    4 : 작성일
+                    5 : 조회수
+                */
+                if(i == 0) {
+                    if($(element).children().length == 1) {
+                        num = 0000; // [공지] 태그가 붙어있으면 0000으로 저장
+                    } else {
+                        num = $(element).text().trim()
                     }
-                })
-            }
-            
+                }
+
+                if(i == 1) {
+                    title = $(element).text().trim()
+
+                    // 실제 board_no 추출
+                    const tag_a = $(element).children().attr("href")
+                    const start_num = tag_a.indexOf('board_no')+9
+                    const end_num = tag_a.indexOf('approve')-1
+                    board_no = tag_a.substring(start_num, end_num)
+                    //console.log(tag_a.substring(start_num, end_num));
+                }
+
+                if(i == 3) {
+                    if($(element).children().length == 1) {
+                        file = 1;
+                    } else {
+                        file = 0;
+                    }
+                }
+
+                if(i == 4) {
+                    date = $(element).text().trim();
+                }
+
+                if(i == 5) {
+                    view = $(element).text().trim();
+                    const dataObj = {
+                        "num" : num,
+                        "board_no" : board_no,
+                        "title" : title,
+                        "file" : file,
+                        "date" : date,
+                        "view" : view
+                    }
+                    page.push(dataObj)
+                }
+            })
         })
+
+        console.log(page);
     } catch(e) {
         console.log(e);
     }
 }
 crawler();
-
