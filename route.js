a74c42db56408bbda57ef4430e631e43b4e60b3d
Seonghyunsun <leaf1993@naver.com>
2020-12-09 16:15:54 +0900

    merge test

--- route.js
+++ route.js
@@ -155,7 +155,6 @@ router.get("/select", (req, res) => {
 })
 
 
-<<<<<<< HEAD
 // BOARD READ - 과목게시판 특정 단어로 글 조회
 // 클라이언트에서 과목명/특정값을 파라미터로 전달하면 해당하는 튜플을 전송한다.
 router.get("/select/:subject/search/:word", (req, res) => {
@@ -196,8 +195,8 @@ router.get("/select/:id", (req, res) => {
 // 클라이언트에서 post_no과 user_id를 파라미터로 전달한다.
 // 작성할 댓글 내용은 body를 통해 전달된다.
 // post_no과 user_id는 외래키로 지정되어 유효하지 않은 값이 전달되면 에러가 발생한다.
-router.post("/reply/insert/:user/:post", (req, res) => {
-    const dataObj = {
+// router.post("/reply/insert/:user/:post", (req, res) => {
+//     const dataObj = {
 
 // BOARD REPLY DELETE - 과목게시판 내 선택한 글 삭제
 // 클라이언트에서 post_no과  전달하면 해당 튜플을 삭제한다.
@@ -214,7 +213,5 @@ router.get("/delete/:id", (req, res) => {
     })
 })
 
-=======
->>>>>>> 7c3497512cdad9644607582aa89409be68a5646e
 
 module.exports = router;
