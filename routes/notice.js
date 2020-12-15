16ff7a46b0180fb23832084a64cb2b9329344de1
jinyoung1129 <jy11290@naver.com>
2020-12-16 03:40:46 +0900

    Add Function
    공지사항 검색 추가

--- routes/notice.js
+++ routes/notice.js
@@ -29,5 +29,30 @@ router.get("/read/:pageNum/:major", (req, res) => {
     })
 })
 
+router.get("/read/:pageNum/:major/:search", (req, res) => {
+    const pageNum = req.params.pageNum; // 몇번째 페이지
+    const major = req.params.major;
+    const search = req.params.search;
+
+    const sql = "SELECT major_code FROM MAJOR WHERE major_name = ?"
+
+    db.query(sql, major, (err, result) => {
+        if(err) {
+            console.log("select err : ", err);
+        }
+        else {
+            console.log("notice selected");
+            const boardType_seq = result[0].major_code;
+            const url = "https://www.gachon.ac.kr/major/bbs.jsp?pageNum="+pageNum+"&pageSize=10&boardType_seq="+String(boardType_seq)+"&approve=&secret=&answer=&branch=&searchopt=title&searchword="+encodeURI(search)
+            
+            console.log(url);
+            noticeController(url).then(result => {
+                res.send(result);
+            })
+            
+        }
+    })
+})
+
 module.exports = router;
 
