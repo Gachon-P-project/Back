cf0cd6a00e3e589babca91a0040ae090d392280e
Seonghyunsun <leaf1993@naver.com>
2020-12-24 00:15:47 +0900

    Test Git

--- routes/reply.js
+++ routes/reply.js
@@ -21,22 +21,7 @@ router.get("/read/:post", replyController.readReply)
 // REREPLY CREATE - 대댓글 작성 (수정중)
 router.get("/rereply/insert/:userNo/:postNo/:replyNo", replyController.createReReply)
 
-<<<<<<< HEAD
 // REREPLY UPDATE - 대댓글 수정 (불가)
 //router.get("/rereply/update/:user/:post", replyController.updateReReply)
-=======
-// REREPLY CREATE - 대댓글 작성
-router.get("/rereply/insert/:user/:post", replyController.createReReply)
-
-// REREPLY UPDATE - 대댓글 수정
-router.get("/rereply/update/:user/:post", replyController.updateReReply)
-
-// // REREPLY DELETE - 대댓글 삭제
-// router.delete("/rereply/delete/:user/:post", replyController.deleteReReply)
-
-// // REREPLY READ - 대댓글 보기
-// router.get("/rereply/read/:user/:post", replyController.readReReply)
-
->>>>>>> 44eab2beb08edb966a746875ecdcb35ae9e948b0
 
 module.exports = router;
