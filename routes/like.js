95dcfafd7351b0cb8d22ef7c49fcfddc8a41793b
Seonghyunsun <leaf1993@naver.com>
2020-12-29 03:29:29 +0900

    ADD: Edit likeController, likeModel, like to include like/like cancel  function

--- routes/like.js
+++ routes/like.js
@@ -5,14 +5,11 @@ const db = mysqlConObj.init();
 
 const likeController = require('../controllers/likeController')
 
-// LIKE CREATE - 좋아요
+// LIKE CREATE - 좋아요, 좋아요 취소
 router.post("/insert/:postNo/:userNo", likeController.createLike)
 
-// LIKE DELETE - 좋아요 취소
-// router.delete("/delete/:postNo/:userNo", likeController.deleteLike)
-
-// LIKE CREATE - 좋아요 개수 조회
-// router.get("/read/:postNo", likeController.readLike)
+// LIKE READ - 좋아요 개수 조회
+router.get("/read/:postNo", likeController.readLike)
 
 
 module.exports = router;
