42999997f83b2d8282a1a36bbfa18e9b9af20ada
Seonghyunsun <leaf1993@naver.com>
2020-12-30 19:37:50 +0900

    Fix: Edit likeController, likeModel, like to include like/like cancel  function

--- models/boardModel.js
+++ models/boardModel.js
@@ -9,7 +9,7 @@ const db = mysqlConObj.init();
 // 클라이언트에서 과목명/교수명을 파라미터로 전달하면 해당하는 튜플을 전송
 exports.readList = (user_no, subject_name, professor_name, cb) => {
     // 게시글 목록에 댓글 개수, 좋아요 개수 출력 
-    const sql = "select b.*, (select count(*) from REPLY where post_no=b.post_no) as reply_cnt, (select count(*) from LIKEBOARD where post_no=b.post_no) as like_cnt, (select count(*) from LIKEBOARD where user_no=?) as like_user from BOARD b where b.subject_name=? and b.professor_name=?";
+    const sql = "select b.*, (select count(*) from REPLY where post_no=b.post_no) as reply_cnt, (select count(*) from LIKEBOARD where post_no=b.post_no) as like_cnt, (select count(*) from LIKEBOARD where user_no=? and post_no=b.post_no) as like_user from BOARD b where b.subject_name=? and b.professor_name=?";
     db.query(sql, [user_no, subject_name, professor_name], (err, results) => {
         if (err) {
             console.log("select err : ", err);
@@ -61,7 +61,7 @@ exports.deleteBoard = (post_no, cb) => {
             console.log("delete err : ", err);
         }
         else {
-            cb(JSON.parse(JSON.stringify(results)));
+            cb(JSON.parse(JSON.stringify(results))); 
         }
     })
 }
@@ -69,8 +69,8 @@ exports.deleteBoard = (post_no, cb) => {
 // BOARD READ - 과목게시판 특정 단어로 글 조회
 // 클라이언트에서 과목명/특정값을 파라미터로 전달하면 해당하는 튜플을 전송한다.
 exports.readSomeList = (subject_name, professor_name, post_word, cb) => {
-    const sql = "SELECT * FROM BOARD WHERE subject_name = ? AND professor_name = ? AND (post_contents LIKE "+ db.escape('%'+post_word+'%')+"RO post_title LIKE "+ db.escape('%'+post_word+'%')+")";
-
+    const sql = "SELECT * FROM BOARD WHERE subject_name = ? AND professor_name = ? AND (post_contents LIKE "+ db.escape('%'+post_word+'%')+"OR post_title LIKE "+ db.escape('%'+post_word+'%')+")";
+    "select b.*, (select count(*) from REPLY where post_no=b.post_no) as reply_cnt, (select count(*) from LIKEBOARD where post_no=b.post_no) as like_cnt, (select count(*) from LIKEBOARD where user_no=?) as like_user from BOARD b WHERE subject_name = ? AND professor_name = ? AND (post_contents LIKE "+ db.escape('%'+post_word+'%')+"RO post_title LIKE "+ db.escape('%'+post_word+'%')+")";
     db.query(sql, [subject_name, professor_name], (err, results) => {
         if (err) {
             console.log("read err : ", err);
