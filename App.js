bd7069cc2683625afecc839fede52be81ec80dd1
jinyoung1129 <jy11290@naver.com>
2021-01-19 00:04:52 +0900

    Fix Function
    path error 시 로그에 originalUrl 추가

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(methodOverride('_mothod'));
 app.use(routes);
 app.use((req, res, next) => {
-    const err_msg = "path error " + new Date().toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})
+    const err_msg = "path error " + new Date().toLocaleString("ko-KR", {timeZone: "Asia/Seoul"}) + " originalUrl : " + req.originalUrl
     console.log(err_msg);
     res.status(404).send(err_msg);
 })
